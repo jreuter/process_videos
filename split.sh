de0504b9e1cda196dd2464ae2dfc00e885a35cc1
Jarrod Reuter <jarrod.reuter@gmail.com>
2020-06-29 21:00:10 -0400

    Splitting the first 2-minutes into 30-sec chunks.
    Also, replacing some values with variables.

--- split.sh
+++ split.sh
@@ -3,29 +3,37 @@
 FILE="$(ls *.mp4)"
 echo $FILE
 FILE_LENGTH="$(ffmpeg -i "$FILE" 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }')"
+EXPECTED_FPS=30
 
 START=0
 SIZE=120
+SSIZE=30
 
-mkdir pieces-120sec
-mkdir pieces-30sec
+mkdir pieces-${SIZE}sec
+mkdir pieces-${SSIZE}sec
 touch failures.txt
 
 #touch fileList.txt
 #echo "" > fileList.txt
 
+# Split the first 2 minutes so any edits at the beginning don't jump to a strange keyframe.
+while [[ $START -lt $SIZE ]]; do
+    ffmpeg -i "$FILE" -ss $START -t $SSIZE -c:v libx264 -crf 20 -r $EXPECTED_FPS -force_key_frames "expr:gte(t,n_forced*1)" pieces-${SSIZE}sec/${START}.mp4
+    let START=START+SSIZE
+done
+
+# Split the rest of the video, check for flaws, etc.
 while [[ $START -lt $FILE_LENGTH ]]; do
-    ffmpeg -i "$FILE" -ss $START -t $SIZE -c copy pieces-120sec/${START}.mp4
+    ffmpeg -i "$FILE" -ss $START -t $SIZE -c copy pieces-${SIZE}sec/${START}.mp4
     # Check file for fps, if less than 30 (variable), create 30-sec clip.
-    FPS="$(ffmpeg -i pieces-120sec/${START}.mp4 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p")"
+    FPS="$(ffmpeg -i pieces-${SIZE}sec/${START}.mp4 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p")"
     echo "FPS for video ${START} is ${FPS}."
-    if [ $(echo "${FPS} < 30" | bc) -ne 0 ]; then
+    if [ $(echo "${FPS} < ${EXPECTED_FPS}" | bc) -ne 0 ]; then
         # add to failures list
-        echo "pieces-120sec/${START}.mp4" >> failures.txt
+        echo "${START}.mp4" >> failures.txt
         SSTART=${START}
-        SSIZE=30
         while [[ $SSTART -lt START+SIZE ]] && [[ $SSTART -lt $FILE_LENGTH  ]]; do
-            ffmpeg -i "$FILE" -ss $SSTART -t $SSIZE -c:v libx264 -crf 20 -r 30 -force_key_frames "expr:gte(t,n_forced*1)" pieces-30sec/${SSTART}.mp4
+            ffmpeg -i "$FILE" -ss $SSTART -t $SSIZE -c:v libx264 -crf 20 -r ${EXPECTED_FPS} -force_key_frames "expr:gte(t,n_forced*1)" pieces-${SSIZE}sec/${SSTART}.mp4
             let SSTART=SSTART+SSIZE
         done
     fi
@@ -34,12 +42,12 @@ while [[ $START -lt $FILE_LENGTH ]]; do
 done
 
 mkdir all-files
-mv -f pieces-120sec/* all-files/
-mv -f pieces-30sec/* all-files/
+mv -f pieces-${SIZE}sec/* all-files/
+mv -f pieces-${SSIZE}sec/* all-files/
 
 ## Just combine the files. Only useful if I can get it to work with flowblade.
 #ffmpeg -f concat -i fileList.txt -c copy ${FILE}-combined.mp4
 
-rmdir pieces-120sec
-rmdir pieces-30sec
+rmdir pieces-${SIZE}sec
+rmdir pieces-${SSIZE}sec
 
